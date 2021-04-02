9481612dc230a1c5681ccd880f529e99c0359207
ozemzami <oussamazemzami@gmail.com>
2021-04-02 23:47:55 +0200

    changing the server architecture

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import app from '@server';
+import logger from '@shared/logger';
+
+
+// Start the server
+const port = Number(process.env.PORT || 3000);
+app.listen(port, () => {
+    logger.info('Express server started on port: ' + port);
+});
